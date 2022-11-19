9def7260506b977759fa5b49e75a05f4e0a7700d
Innova Tech <108552026+InnovaTechUY@users.noreply.github.com>
2022-11-19 01:31:38 -0300

    Add files via upload

---
+++ PhpArtisanServe.bat
@@ -0,0 +1,14 @@
+cd ApiAutenticacion
+start php artisan serve --port=8000 
+cd ..
+cd ApiPublicidad
+start php artisan serve --port=8001
+cd ..
+cd ApiResultados
+start php artisan serve --port=8002
+cd..
+cd AppUsuario
+start php artisan serve --port=8003
+cd..
+cd BackOffice
+start php artisan serve --port=8004
